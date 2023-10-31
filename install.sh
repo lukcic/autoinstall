2aa93b6a039edda0427658bc48cbf4dfdb82cd3a
lukcic <lukcic@int.pl>
2023-10-31 13:41:10 +0100

    ZSH autoinstall

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+apt install git zsh vim tmux bat gh -y
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+mv ~/.zshrc ~/.zshrc.bak
+cp ./.zshrc ~/.zshrc
+mv ~/.p10k.zsh ~/.p10k.zsh.bak
+cp ./.p10k.zsh ~/.p10k.zsh
+
+chsh -s $(which zsh)
