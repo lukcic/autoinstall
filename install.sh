c66215a6be1c04d31cf0e16b64de12b80165c8a0
lukcic <lukcic@int.pl>
2023-10-31 13:58:08 +0100

    Autoinstall script error while backup rc files fix

--- install.sh
+++ install.sh
@@ -9,9 +9,9 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
-mv ~/.zshrc ~/.zshrc.bak
-cp ./.zshrc ~/.zshrc
-mv ~/.p10k.zsh ~/.p10k.zsh.bak
+mv ~/.zshrc ~/.zshrc.bak || true
+cp ./.zshrc ~/.zshrc 
+mv ~/.p10k.zsh ~/.p10k.zsh.bak || true
 cp ./.p10k.zsh ~/.p10k.zsh
 
 chsh -s $(which zsh)
